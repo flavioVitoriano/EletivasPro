924bae0b59396928cae0784e3d4faa121f6779d7
luckysora <flavioisaalien@gmail.com>
2018-11-25 10:19:36 -0300

    adicionado django ao virtualenv

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/flavio/Projetos/EletivasPro/venv/bin/python3
+# EASY-INSTALL-SCRIPT: 'Django==2.1.3','django-admin.py'
+__requires__ = 'Django==2.1.3'
+__import__('pkg_resources').run_script('Django==2.1.3', 'django-admin.py')
